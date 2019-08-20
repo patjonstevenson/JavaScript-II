c1f3199b2fb96736cc2b387f5a0d8292460e303f
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-08-20 13:26:27 -0700

    finishde callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -38,32 +38,52 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+console.log(getLength(items, x => `Length is ${x}`));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+console.log(last(items, x => `Last is ${x}`));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+console.log(sumNums(1, 2, x => `Sum is ${x}`));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+console.log(multiplyNums(10, 4, x => `Product is ${x}`));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.filter(listItem => listItem === item).length > 0);
 }
+console.log(contains("yo-yo", items, x => `Contains? ${x}`));
 
 /* STRETCH PROBLEM */
+
+const stretchItems = ["yo-yo"].concat(items);
+console.log(stretchItems);
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  return cb(
+    array.reduce(function(newList, item) {
+      if (newList.filter(listItem => listItem === item).length === 0) {
+        newList.concat(item);
+      }
+    }, [])
+  );
 }
+console.log(removeDuplicates(stretchItems, x => `New list is ${x}`));
